854f6d38cdc6b2c13a1e3aa56deb114ee8e1db58
Didier Roque Ginebra <didorg@gmail.com>
2025-06-28 15:57:33 -0400

    [qrg-main-1.0.0]Feat: - Added a deploy script to package.json for streamlined build and deployment process. - Set the base path in vite.config.ts for proper asset resolution in production.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/qr-code-generator/',
   server: {
     port: 5156,
     host: true,
